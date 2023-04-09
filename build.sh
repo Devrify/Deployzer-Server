bc64e4c299cc24bcb1db78f88d229d330a23c6a7
houance <837507557@qq.com>
2023-04-09 23:09:54 +0800

    增加推送镜像到仓库

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 image_name=python/pipeline
+repository_tag=python-pipeline
+repository_name=nopepsi/app
 container_name=pipeline
 ssh_path_in_container=/root/.ssh
 ssh_path_in_host=~/.ssh
@@ -13,4 +15,6 @@ cd /root/pythonProject/lightweight-pipeline
 git pull
 kill_container || true
 docker build -t ${image_name} .
+docker tag ${image_name}:latest ${repository_name}:${repository_tag}
+docker push ${repository_name}:${repository_tag}
 docker run -d -p 4400:4400 --restart always --name ${container_name} -v ${ssh_path_in_host}:${ssh_path_in_container}:ro ${image_name}
