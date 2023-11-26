5ca4250c88a2fa6cfdc11e2838120a287d9bcb01
houance <837507557@qq.com>
2023-11-26 23:55:44 +0800

    初始化工程

---
+++ src/main/java/com/devrify/deployzerserver/DeployzerServerApplication.java
@@ -0,0 +1,13 @@
+package com.devrify.deployzerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployzerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployzerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devrify/deployzerserver/DeployzerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrify.deployzerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployzerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
