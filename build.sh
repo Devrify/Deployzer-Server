ad2644cdd28899a0903137af7602084a6a7b2c18
houance <837507557@qq.com>
2023-12-27 00:01:07 +0800

    增加打包运行语句

---
+++ build.sh
@@ -0,0 +1,6 @@
+image_name="nopepsi/app:deployzer-server";
+container_name="deployzer-server";
+docker rm -f $container_name;
+docker image rm -f $image_name;
+docker build -t $image_name .;
+docker run -p 10810:8080 --name $container_name $image_name;
