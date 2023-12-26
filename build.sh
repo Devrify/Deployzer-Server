a4cfa4d11b153060a107857bbed1572161eddddb
houance <837507557@qq.com>
2023-12-27 00:35:35 +0800

    增加打包运行语句

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ container_name="deployzer-server";
 docker rm -f $container_name;
 docker image rm -f $image_name;
 docker build -t $image_name .;
-docker run -it -p 10810:8080 --name $container_name $image_name;
+docker run -d -p 10810:8080 --name $container_name $image_name;
